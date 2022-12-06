ff8d80932a14aff9ab6ae6b9134e185d1bd5fd2d
jamiestungo <jamiestungo@gmail.com>
2022-12-06 15:43:00 +0000

    Changed case on PORT environment variable

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ import {router} from './routes/index.js'
 import cors from 'cors'
 
 export const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT;
 // nothing
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/", router);
 app.listen(PORT, function () {
