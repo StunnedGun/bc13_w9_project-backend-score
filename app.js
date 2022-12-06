6824cb5af6285373bdc7445b572df1624174bd63
jamiestungo <jamiestungo@gmail.com>
2022-12-06 15:11:08 +0000

    Changes to port number again... reverted

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ import {router} from './routes/index.js'
 import cors from 'cors'
 
 export const app = express();
-//const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3001;
 // nothing
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/", router);
-/*app.listen(PORT, function () {
+app.listen(PORT, function () {
  console.log(`Server is running on port ${PORT}`);
-});*/
+});
 /*
 app.listen(function () {
     console.log(`Server is running on port ${PORT}`);
