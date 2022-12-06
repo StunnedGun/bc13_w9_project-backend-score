b8b9bad8ed13ec68f31edd1824b85e4467c8519f
jamiestungo <jamiestungo@gmail.com>
2022-12-06 14:47:20 +0000

    Removed the app.listen

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.use("/api/", router);
-app.listen(PORT, function () {
+/*app.listen(PORT, function () {
  console.log(`Server is running on port ${PORT}`);
-});
+});*/
+/*
+app.listen(function () {
+    console.log(`Server is running on port ${PORT}`);
+   });*/
 
